13d2fbac983ea787a397856c79125ae571083798
ZHENLI <31942901+zhenlili144@users.noreply.github.com>
2017-10-18 20:21:29 +0800

    Add files via upload

---
+++ jianli.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+
+clear
+echo  '--------------------------------------------------------'
+
+#a=(- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -)
+
+#for i in ${a[@]}
+#do
+     #echo $i
+#done
+
+echo -n "                 "
+
+a=("简" "历" "")   
+
+for i in  ${a[@]}
+#$RANDOM%a
+do
+
+      sleep 0.5 
+echo -n $i
+  
+
+
+
+#function mz()
+#{
+
+#   sleep 1  
+ #  echo -n "我"
+#   sleep 1 
+#   echo -n "的"
+#   sleep 1
+#   echo -n "名"
+#   sleep 1
+#   echo -n "子"
+#   sleep 1
+#   echo -n "叫"
+#   sleep 1
+ #  echo -n "甄"
+ #  sleep 1
+ #  echo -n "理"
+#}
+
+
+#sleep 1
+#echo  "$mz"
+
+done  
+
+echo   
+
+
+a=("姓" "名" ":" 甄 理 )
+for i in  ${a[@]}
+#$RANDOM%a
+do
+
+      sleep 0.5
+echo -n $i
+done 
+
+
+
+
+
+
+
+echo   
+
+
+a=( 性 别 : 男                                )
+for i in  ${a[@]}
+#$RANDOM%a
+do
+
+      sleep 0.5
+echo -n $i
+done
+
+echo -n    "                     "
+a=(民 族 ： 汉)
+for i in ${a[@]}
+do 
+        sleep 0.5
+echo -n $i
+done
+echo
+#echo -n   "                      "
+
+a=(年 龄 3 0 )
+for i in  ${a[@]}
+#$RANDOM%a
+do
+
+      sleep 0.5
+echo -n $i
+done
+  echo -n  "                      "
+
+a=(籍 贯 : 内 蒙 古 根 河 市)
+for i in  ${a[@]}
+#$RANDOM%a
+do
+
+      sleep 0.5
+   echo -n $i
+done
+a=( 联 系 电 话 1 7 6 1 4 9 7 2 3 3 5 )
+for i in  ${a[@]}
+do
+       sleep 0.5
+echo -n $i
+done
+a=(通讯地址 ： 北 京 市 朝 阳 区 劲 松 南 路 华 饮 兄 弟 院 内)
+for i in ${a[@]}
+do
+    sleep 0.5
+echo -n $i
+done
+
+
+
+echo  '--------------------------------------------------------'
